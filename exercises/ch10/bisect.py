9bbb0719d136bddb1c9144db49b6064796e28cc3
Jacob Hernandez <jacob.hernandez@nubank.com.br>
2022-06-07 17:09:51 -0500

    Added function test

--- exercises/ch10/bisect.py
+++ exercises/ch10/bisect.py
@@ -5,7 +5,21 @@ def in_bisect(e, t):
     n = len(t)
     if n <= 4:
         return e in t
-    if e < t[n//2]:
+
+    if e == t[n//2]:
+        return True
+    elif e < t[n//2]:
         return in_bisect(e, t[:n//2])
     else:
         return in_bisect(e, t[n//2:])
+
+
+fin = open("./exercises/resources/words.txt")
+
+t1 = []
+
+for line in fin:
+    word = line.strip()
+    t1.append(word)
+
+print(in_bisect("hippo", t1))
